e7d6a8bb571dbe25872e222614353450edc30adb
zihluwang <really@zihlu.wang>
2025-02-23 17:18:03 +0800

    feat(web): get current ledger in store

---
+++ clear-ledger-web/src/components/transaction-table.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-table>
+    <el-table-column label="交易时间"></el-table-column>
+    <el-table-column label="交易时间"></el-table-column>
+  </el-table>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less"></style>
--- clear-ledger-web/src/store/ledger-store.ts
+++ clear-ledger-web/src/store/ledger-store.ts
@@ -1,10 +1,18 @@
+import { ref, computed } from "vue"
+import { useRoute } from "vue-router"
 import { defineStore } from "pinia"
-import { ref } from "vue"
 import { Ledger } from "@/types"
 
 export const useLedgerStore = defineStore("ledger-store", () => {
   const ledgers = ref<Record<string, Ledger>>({})
-  return { ledgers }
+
+  const getCurrentLedger = computed(() => {
+    const route = useRoute()
+    const ledgerId = route.params.ledgerId as string
+    return ledgers.value[ledgerId] ?? null
+  })
+
+  return { ledgers, getCurrentLedger }
 }, {
   persist: true
 })
