9eb085ae6848c33926165666af4dc06734c38951
zihluwang <really@zihlu.wang>
2025-03-02 18:44:42 +0800

    feat(web-react): login page

--- clear-ledger-web-react/src/layout/empty-layout/index.tsx
+++ clear-ledger-web-react/src/layout/empty-layout/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router"
+import "./index.scss"
 
 export const EmptyLayout = () => {
   return (
-    <>
+    <div className="layout-wrapper">
       <Outlet />
-    </>
+    </div>
   )
 }
--- clear-ledger-web-react/src/store/index.ts
+++ clear-ledger-web-react/src/store/index.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "@/store/auth-slice.ts"
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer
   }
 })
 
