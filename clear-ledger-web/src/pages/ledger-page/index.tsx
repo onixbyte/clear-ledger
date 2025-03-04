a187230190b7974f87e0f68a49f7635b88b03bcd
zihluwang <really@zihlu.wang>
2025-03-04 22:15:32 +0800

    fix: build error

---
+++ clear-ledger-web/src/pages/ledger-page/index.tsx
@@ -0,0 +1,5 @@
+import "./index.scss"
+
+export const LedgerPage = () => {
+  return (<div></div>)
+}
--- clear-ledger-web/vite.config.ts
+++ clear-ledger-web/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import { fileURLToPath } from "node:url"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
