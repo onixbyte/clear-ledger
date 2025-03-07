885a05f94a51bb5d4567fd5c90c8388a5140e484
siujamo <jamo.siu@outlook.com>
2025-03-07 14:34:34 +0800

    refactor: extracted DateTimeFormatters as static constants

--- clear-ledger-server/src/main/java/com/onixbyte/clearledger/guid/LedgerIdCreator.java
+++ clear-ledger-server/src/main/java/com/onixbyte/clearledger/guid/LedgerIdCreator.java
@@ -2,26 +2,26 @@ package com.onixbyte.clearledger.guid;
 
 import com.onixbyte.clearledger.constant.IdType;
 import com.onixbyte.clearledger.service.SerialService;
+import com.onixbyte.clearledger.utils.Formatters;
 import com.onixbyte.guid.GuidCreator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Component
 public class LedgerIdCreator implements GuidCreator<String> {
 
-    private final DateTimeFormatter dateFormatter;
     private final SerialService serialService;
 
-    public LedgerIdCreator(DateTimeFormatter dateFormatter, SerialService serialService) {
-        this.dateFormatter = dateFormatter;
+    @Autowired
+    public LedgerIdCreator(SerialService serialService) {
         this.serialService = serialService;
     }
 
     @Override
     public String nextId() {
-        var date = LocalDate.now().format(dateFormatter);
+        var date = LocalDate.now().format(Formatters.SHORTENED_DATE_FORMATTER);
         return "%s%s%04d".formatted(IdType.LEDGER.getCode(), date, serialService.nextSerial("ledger"));
     }
 }
--- clear-ledger-server/src/main/java/com/onixbyte/clearledger/guid/UserIdCreator.java
+++ clear-ledger-server/src/main/java/com/onixbyte/clearledger/guid/UserIdCreator.java
@@ -2,26 +2,26 @@ package com.onixbyte.clearledger.guid;
 
 import com.onixbyte.clearledger.constant.IdType;
 import com.onixbyte.clearledger.service.SerialService;
+import com.onixbyte.clearledger.utils.Formatters;
 import com.onixbyte.guid.GuidCreator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Component
 public class UserIdCreator implements GuidCreator<String> {
 
-    private final DateTimeFormatter dateFormatter;
     private final SerialService serialService;
 
-    public UserIdCreator(DateTimeFormatter dateFormatter, SerialService serialService) {
-        this.dateFormatter = dateFormatter;
+    @Autowired
+    public UserIdCreator(SerialService serialService) {
         this.serialService = serialService;
     }
 
     @Override
     public String nextId() {
-        var date = LocalDate.now().format(dateFormatter);
+        var date = LocalDate.now().format(Formatters.SHORTENED_DATE_FORMATTER);
         return "%s%s%04d".formatted(IdType.USER.getCode(), date, serialService.nextSerial("user"));
     }
 
