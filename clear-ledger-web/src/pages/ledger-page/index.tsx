e4b8990d8d1eff350f2fbfb58447c572022f6267
zihluwang <really@zihlu.wang>
2025-03-08 13:29:52 +0800

    feat(web): transaction page

--- clear-ledger-web/src/pages/ledger-page/index.tsx
+++ clear-ledger-web/src/pages/ledger-page/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { Transaction } from "@/types"
 import * as TransactionApi from "@/api/transaction"
-import { message } from "antd"
+import { message, Table } from "antd"
 import { AxiosError } from "axios"
 
 type PaginationParams = {
@@ -18,13 +18,21 @@ export const LedgerPage = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([])
   const [paginationParam, setPaginationParams] = useState<PaginationParams>({
     pageNumber: 1,
-    pageSize: 20,
+    pageSize: 10,
     totalPage: 0,
     totalRow: 0,
   })
 
+  const onPageChange = (pageNum: number, pageSize: number) => {
+    setPaginationParams((prev) => ({
+      ...prev,
+      pageNumber: pageNum,
+      pageSize,
+    }))
+  }
+
   useEffect(() => {
-    const fetchTransactions = async () => {
+    ;(async () => {
       if (!ledgerId) return
 
       try {
@@ -47,10 +55,42 @@ export const LedgerPage = () => {
             : "创建账本失败"
         )
       }
-    }
-
-    fetchTransactions()
+    })()
   }, [ledgerId, paginationParam.pageNumber, paginationParam.pageSize])
 
-  return <div>Ledger ID = {ledgerId}</div>
+  return (
+    <div>
+      <Table<Transaction>
+        dataSource={transactions}
+        pagination={{
+          pageSize: paginationParam.pageSize,
+          current: paginationParam.pageNumber,
+          total: paginationParam.totalRow,
+          onChange: onPageChange,
+        }}>
+        <Table.Column<Transaction> title="ID" dataIndex="id" key="id" />
+        <Table.Column<Transaction>
+          title="用户名"
+          dataIndex="username"
+          key="username"
+        />
+        <Table.Column<Transaction>
+          title="金额"
+          key="amount"
+          render={(value: Transaction) => {
+            return `¥ ${value.amount / 100}`
+          }}
+        />
+        <Table.Column<Transaction>
+          title="账单描述"
+          dataIndex="description"
+          key="amount"
+        />
+        <Table.Column<Transaction>
+          title="交易时间"
+          dataIndex="transactionDate"
+        />
+      </Table>
+    </div>
+  )
 }
